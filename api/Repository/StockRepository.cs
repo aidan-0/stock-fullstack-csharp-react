c7f7cb237ac26e42d7c1291fbfd9e94e9cecfc04
Aidan <am.mcdonald00@gmail.com>
2024-10-14 18:07:48 +0800

    comments tied to user names

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -43,7 +43,7 @@ namespace api.Repository
 
         public async Task<List<Stock>> GetAllAsync(QueryObject query)
         {
-            var stocks = _context.Stocks.Include(c => c.Comment).AsQueryable();
+            var stocks = _context.Stocks.Include(c => c.Comment).ThenInclude(a => a.AppUser).AsQueryable();
 
             if(!string.IsNullOrWhiteSpace(query.CompanyName))
             {
